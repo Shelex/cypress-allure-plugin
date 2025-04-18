946b5c234506dd05e5a6097934c1540306cf8dd1
Kar Dam <netkardam@gmail.com>
2025-04-18 20:13:37 +0200

    add the support of reran mid-test due to a cy.visit

--- reporter/patching.js
+++ reporter/patching.js
@@ -1,20 +1,41 @@
 const logger = require('./debug');
 
-const ALLURE_AFTER_HOOK = '__allure_after_hook__';
-const ALLURE_AFTER_EACH_HOOK = '__allure_after_each_hook__';
+const ALLURE_HOOK_NAME = {
+    after: '__allure_after_hook__',
+    afterEach: '__allure_after_each_hook__',
+    before: '__allure_before_hook__',
+    beforeEach: '__allure_before_each_hook__'
+};
 
-const isAllureAfterHook = (nameOrFn) => nameOrFn.includes(ALLURE_AFTER_HOOK);
+/**
+ * isAllureSpecificHook
+ * @param {string} hookName
+ * @param  {Array<String>} type - can be after/afterEach/before/beforeEach
+ * @return true/false
+ */
+const isAllureSpecificHook = (hookName, ...type) => {
+    if (!hookName) {
+        return false;
+    }
+    let keys = type.flat(Infinity);
+    if (keys.length === 0) {
+        keys = Object.keys(ALLURE_HOOK_NAME);
+    }
+    keys = keys.filter((key) => key in ALLURE_HOOK_NAME);
+    return keys.some((key) => hookName.includes(ALLURE_HOOK_NAME[key]));
+};
 
-const isAllureAfterEachHook = (nameOrFn) =>
-    nameOrFn.includes(ALLURE_AFTER_EACH_HOOK);
-
-const isAllureHook = (nameOrFn) =>
-    isAllureAfterHook(nameOrFn) || isAllureAfterEachHook(nameOrFn);
+const isAllureHook = (hookName) => isAllureSpecificHook(hookName);
 
 const reorderAllureHooksArray = (hooksArray) => {
     let commonHooks = hooksArray.filter((hook) => !isAllureHook(hook.title));
-    let allureHooks = hooksArray.filter((hook) => isAllureHook(hook.title));
-    return [...commonHooks, ...allureHooks];
+    let allureBeforeHooks = hooksArray.filter((hook) =>
+        isAllureSpecificHook(hook.title, 'before', 'beforeEach')
+    );
+    let allureAfterHooks = hooksArray.filter((hook) =>
+        isAllureSpecificHook(hook.title, 'after', 'afterEach')
+    );
+    return [...allureBeforeHooks, ...commonHooks, ...allureAfterHooks];
 };
 
 const reorderAllureHooksGeneric = (
@@ -41,11 +62,10 @@ const reorderAllureHooksGeneric = (
     }
 };
 
-const clearAllureHookSteps = (test, allureLogHooks = false) => {
+const clearTestAllureHookSteps = (test, allureLogHooks = false) => {
     if (!(test && test.steps && test.steps.length)) {
         return test;
     }
-
     test.steps = test.steps.reduce((steps, step) => {
         if (
             isAllureHook(step.name) &&
@@ -68,12 +88,12 @@ const clearAllureHookSteps = (test, allureLogHooks = false) => {
 const clearAllureHookStepsFromTests = (tests, allureLogHooks = false) => {
     if (tests) {
         tests.forEach((test) => {
-            clearAllureHookSteps(test, allureLogHooks);
+            clearTestAllureHookSteps(test, allureLogHooks);
         });
     }
 };
 
-const reorderAllureAfterHooks = (suite) => {
+const reorderAllureAfterAllHooks = (suite) => {
     reorderAllureHooksGeneric(suite, 'afterAll');
 };
 
@@ -81,13 +101,18 @@ const reorderAllureAfterEachHooks = (suite) => {
     reorderAllureHooksGeneric(suite, 'afterEach');
 };
 
+const reorderAllureBeforeAllHooks = (suite) => {
+    reorderAllureHooksGeneric(suite, 'beforeAll');
+};
+
 const reorderAllureGlobalHooks = (suite) => {
     reorderAllureHooksGeneric(suite, 'hooks', '');
 };
 
 const reorderAllureHooks = (suite) => {
+    reorderAllureBeforeAllHooks(suite);
     reorderAllureAfterEachHooks(suite);
-    reorderAllureAfterHooks(suite);
+    reorderAllureAfterAllHooks(suite);
     reorderAllureGlobalHooks(suite);
 };
 
@@ -110,11 +135,12 @@ const isLastRootHook = () => {
     return lastHook && lastHook.hookId === currentRunnable.hookId;
 };
 
-const setTestIdToScreenshot = (allureEnabled, mochaIdToAllure, details) => {
-    if (allureEnabled) {
+const setTestIdToScreenshot = ({ allureEnabled, allure, details }) => {
+    if (allureEnabled && allure && allure.reporter) {
         let currentTest = cy.state('test');
+        let { mochaIdToAllure } = allure.reporter;
         if (details && currentTest && mochaIdToAllure) {
-            const testIds = mochaIdToAllure[currentTest && currentTest.id];
+            const testIds = mochaIdToAllure[currentTest.id];
             if (testIds) {
                 details['testId'] = currentTest.id;
                 let testId = testIds.at(-1);
@@ -127,16 +153,84 @@ const setTestIdToScreenshot = (allureEnabled, mochaIdToAllure, details) => {
     }
 };
 
+const buildPendingResults = ({ currentSpec, allure }) => {
+    return {
+        currentSpec: currentSpec,
+        allureMapping: allure.reporter.mochaIdToAllure,
+        allureResults: allure.reporter.runtime.config,
+        allureResultContainer: allure.reporter.suites.map(
+            (suite) => suite.testResultContainer
+        )
+    };
+};
+
+const clearAllureHookStepsFromHook = ({
+    hook,
+    reporter,
+    allureLogHooks = false
+}) => {
+    if (!hook || !reporter) {
+        return;
+    }
+    let { currentHook } = reporter;
+    if (currentHook && !allureLogHooks && isAllureHook(hook.title)) {
+        // remove allure hook steps so hook will not be kept in the report
+        currentHook.info && (currentHook.info['steps'] = []);
+        currentHook.stepResult && (currentHook.stepResult['steps'] = []);
+    }
+};
+
+const loadPendingResults = ({
+    currentSpec,
+    allureResults,
+    allureMapping,
+    allureResultContainer
+}) => {
+    if (!currentSpec) {
+        return;
+    }
+    let currentRunningSpec = Cypress.spec.relative || Cypress.spec.absolute;
+    if (currentRunningSpec === currentSpec) {
+        if (
+            allureMapping &&
+            Object.keys(Cypress.Allure.reporter.mochaIdToAllure || {})
+                .length === 0
+        ) {
+            Cypress.Allure.reporter.mochaIdToAllure = allureMapping;
+        }
+        if (allureResults && allureResults.writer) {
+            Object.keys(allureResults.writer).forEach((key) => {
+                Cypress.Allure.reporter.runtime.config.writer[key] =
+                    allureResults.writer[key];
+            });
+        }
+        if (allureResultContainer) {
+            Cypress.Allure.reporter.runtime.config.writer['groups'].push(
+                ...allureResultContainer
+            );
+        }
+    } else {
+        cy.task(
+            'savePendingAllureResults',
+            {},
+            {
+                log: config.logAllureHooksEnabled()
+            }
+        ).then((pendingResults) => {
+            logger.allure(
+                '########### Reinit pendingResults ################ %O',
+                pendingResults
+            );
+        });
+    }
+};
 // Export everything
 module.exports = {
-    ALLURE_AFTER_HOOK,
-    ALLURE_AFTER_EACH_HOOK,
-    isAllureHook,
-    clearAllureHookStepsFromTests,
-    reorderAllureAfterHooks,
-    reorderAllureAfterEachHooks,
-    reorderAllureGlobalHooks,
+    ALLURE_HOOK_NAME,
     reorderAllureHooks,
-    isLastRootHook,
-    setTestIdToScreenshot
+    setTestIdToScreenshot,
+    buildPendingResults,
+    loadPendingResults,
+    clearAllureHookStepsFromTests,
+    clearAllureHookStepsFromHook
 };
